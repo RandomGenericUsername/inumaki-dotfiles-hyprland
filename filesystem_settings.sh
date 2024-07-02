62f48a918a3bb968b467c8af776957262b49ce05
Juan David <jdavidth.01@gmail.com>
2024-07-02 02:29:57 -0500

    advanced with wallpaper script

--- filesystem_settings.sh
+++ filesystem_settings.sh
@@ -3,7 +3,8 @@
 
 ###################################### Define global variables for the setup and installation ########################################
 
-export ENV_NAME=".inumaki-dotfiles-env"
+export ENV_NAME_RAW="inumaki-dotfiles-env"
+export ENV_NAME=".$ENV_NAME_RAW"
 export ENV_INSTALL_PATH="$HOME"
 export ENV_DIR="$ENV_INSTALL_PATH/$ENV_NAME"
 export CACHE_DIR="$ENV_DIR/.cache"
@@ -12,21 +13,27 @@ export SETTINGS_DIR="$ENV_DIR/.settings"
 export WALLPAPER_DIR="$ENV_DIR/wallpapers"
 
 
-
 ###################################### YYY ########################################
 
-export HYPRLAND_DIR="$CONFIG_DIR/hypr"
-export HYPRLAND_SCRIPTS_DIR="$HYPRLAND_DIR/scripts"
 
 
 ###################################### YYY ########################################
-export HYPRPAPER_CACHE_DIR="$CACHE_DIR/hyprpaper"
-export HYPRPAPER_CACHED_USED_WALLPAPER="$HYPRPAPER_CACHE_DIR/used-wallpaper"
-export HYPRPAPER_CACHED_CURRENT_WALLPAPER="$HYPRPAPER_CACHE_DIR/current-wallpaper"
-export HYPRPAPER_DEFAULT_SPLASH='false'
+#export HYPRPAPER_CACHE_DIR="$CACHE_DIR/hyprpaper"
+#export HYPRPAPER_CACHED_USED_WALLPAPER="$HYPRPAPER_CACHE_DIR/used-wallpaper"
+#export HYPRPAPER_CACHED_CURRENT_WALLPAPER="$HYPRPAPER_CACHE_DIR/current-wallpaper"
+#export HYPRPAPER_CACHED_BLURRED_WALLPAPER="$HYPRPAPER_CACHE_DIR/blurred_wallpaper.png"
+#export HYPRPAPER_CACHED_SQUARE_WALLPAPER="$HYPRPAPER_CACHE_DIR/square_wallpaper.png"
+#export HYPRPAPER_CACHED_CURRENT_WALLPAPER_RASI_CONFIG="$HYPRPAPER_CACHE_DIR/current_wallpaper.rasi"
+#export HYPRPAPER_CACHED_BLUR_FILE="$SETTINGS_DIR/blur.sh"
+#export HYPRPAPER_DEFAULT_SPLASH='false'
+#
+#export WAL_CACHE_DIR="$CACHE_DIR/wal"
+#export WAL_CURRENT_COLORS="$WAL_CACHE_DIR/colors.sh"
 
 
-
+###################################### YYY ########################################
+#export USER_SETTINGS_DIR="$HOME/$ENV_NAME_RAW"
+#export USER_SETTINGS="$HOME/$ENV_NAME_RAW/settings.sh"
 ###################################### YYY ########################################
 # This are the directories that will be copied to the $ENV_INSTALL_PATH
 export project_slug="$ENV_NAME"
--- fs/{{cookiecutter.project_slug}}/.config/hypr/scripts/wallpaper-effects.sh
+++ fs/{{cookiecutter.project_slug}}/.config/hypr/scripts/wallpaper-effects.sh
@@ -1,12 +1,12 @@
     #!/bin/bash
 
     # Open rofi to select the Hyprshade filter for toggle
-    options="$(ls ~/dotfiles/inumaki/hypr/effects/wallpaper/)\noff"
+    options="$(ls {{cookiecutter.CONFIG_DIR}}/hypr/effects/wallpaper/)\noff"
     
     # Open rofi
-    choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/inumaki/rofi/config-themes.rasi -i -no-show-icons -l 5 -width 30 -p "Hyprshade") 
+    choice=$(echo -e "$options" | rofi -dmenu -replace -config {{cookiecutter.CONFIG_DIR}}/rofi/config-themes.rasi -i -no-show-icons -l 5 -width 30 -p "Hyprshade") 
     if [ ! -z $choice ] ;then
-        echo "$choice" > ~/dotfiles/inumaki/.settings/wallpaper-effect.sh
+        echo "$choice" > {{cookiecutter.SETTINGS_DIR}}/wallpaper-effect.sh
         dunstify "Changing Wallpaper Effect to " "$choice"
-        ~/dotfiles/inumaki/hypr/scripts/wallpaper.sh init
+        {{cookiecutter.CONFIG_DIR}}/hypr/scripts/wallpaper.sh init
     fi
