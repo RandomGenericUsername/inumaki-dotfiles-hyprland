1680cffb3e5965933355cd71bb98ee60c7fed7ee
Juan David <jdavidth.01@gmail.com>
2024-07-03 15:30:52 -0500

    refactor

--- fs/{{cookiecutter.project_slug}}/.config/hypr/scripts/apply_waybar_theme.sh
+++ fs/{{cookiecutter.project_slug}}/.config/hypr/scripts/apply_waybar_theme.sh
@@ -1,30 +1,32 @@
-#!/bin/bash
-#  ____  _             _    __        __          _                 
-# / ___|| |_ __ _ _ __| |_  \ \      / /_ _ _   _| |__   __ _ _ __  
-# \___ \| __/ _` | '__| __|  \ \ /\ / / _` | | | | '_ \ / _` | '__| 
-#  ___) | || (_| | |  | |_    \ V  V / (_| | |_| | |_) | (_| | |    
-# |____/ \__\__,_|_|   \__|    \_/\_/ \__,_|\__, |_.__/ \__,_|_|    
-#                                           |___/                   
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
+kill_waybar() {
+    echo "Attempting to kill all Waybar instances"
+
+    # Check if any Waybar instances are running
+    if pgrep waybar > /dev/null; then
+        echo "Waybar is running, terminating instances..."
+        killall waybar
+        pkill waybar
+        sleep 0.2
+        echo "Waybar instances terminated"
+    else
+        echo "No Waybar instances are running"
+    fi
+}
+
+
 waybar_cached_status={{cookiecutter.CACHE_DIR}}/waybar-disabled
 theme_style_cfg={{cookiecutter.CACHE_DIR}}/.themestyle.sh
 
 # Check if waybar-disabled file exists
 if [ -f $waybar_cached_status ] ;then 
-    echo "Disabling waybar"
-    killall waybar
-    pkill waybar
+    kill_waybar
     exit 1 
 fi
 
 # ----------------------------------------------------- 
 # Quit all running waybar instances
 # ----------------------------------------------------- 
-#echo "Killing all instances of waybar"
-#killall waybar
-#pkill waybar
-#sleep 0.2
+kill_waybar
 
 # ----------------------------------------------------- 
 # Default theme: /THEMEFOLDER;/VARIATION
