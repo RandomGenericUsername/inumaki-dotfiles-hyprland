7a23fea8821ea6055ee2625de96373eec20bfeae
Juan David <jdavidth.01@gmail.com>
2024-10-27 04:40:22 -0500

    Updated script for checking distro

--- src/.lib/check_distro.sh
+++ src/.lib/check_distro.sh
@@ -1,21 +1,24 @@
-check_distro() {
+check_distro(){
     local file_path="$1"
 
     # Check if the file exists
     if [[ ! -f "$file_path" ]]; then
-        echo "Error checking valid linux distro: File not found at: '$file_path'"
+        echo "Error: File not found at '$file_path'"
         exit 1
     fi
 
-    # Get the current distribution
-    current_distro=$(grep '^ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+    # Get ID and ID_LIKE from /etc/os-release
+    local distro_id
+    local distro_like
+    distro_id=$(grep '^ID=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+    distro_like=$(grep '^ID_LIKE=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
 
-    # Check if the current distribution is in the file
-    if grep -q "^$current_distro$" "$file_path"; then
-        #echo "Distribution '$current_distro' is supported."
-        return 0
+    # Determine base distro and check against supported list
+    if grep -q "^$distro_like$" "$file_path"; then
+        echo "$distro_like"
+    elif grep -q "^$distro_id$" "$file_path"; then
+        echo "$distro_id"
     else
-        echo "Error: Distribution '$current_distro' is not supported."
-        exit 2
+        echo ""
     fi
 }
